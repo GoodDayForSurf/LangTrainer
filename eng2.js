47ed0888bd123d06daee6aa71513069d9830ad3f
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2023-02-07 01:19:01 +0400

    add new list

---
+++ eng2.js
@@ -0,0 +1,88 @@
+export const DATA  = `
+У меня кружится голова. Тут душно.
+I feel dizzy! it's stuffy here.
+
+Это разительно отличается от моей предыдущей работы.
+It's strikingly different from my previous work.
+
+Я изо всех сил пытаюсь понять.
+I'm struggling to understand.
+
+Что толку жаловаться?
+What's the use of complaining?
+
+Я еще не разобрался.
+I have not figured it out yet.
+
+Мы все преодолеем месте.
+We will overcome everything together.
+
+Я не мог удержаться.
+I couldn't help myself
+
+Это нанесло серьезный ущерб.
+It caused serious damage.
+
+Не связывайся с ними.
+Don't get involved with them.
+
+I kept trying to persuade him.
+Я продолжал уговаривать его.
+
+Не оправдывайся!
+Don't make excuses!
+
+Это только вопрос времени.
+It's only a matter of time.
+
+Я не могу не заметить это.
+I can't help but notice it.
+
+Я прошел через это (покончил с этим)
+I'm through with it. 
+I'm done with it.
+
+Он сделал это из ревности (зависти, жадности)
+He did it out of jealousy (envy, greed)
+
+Я серьезно.
+I mean it. (I'm serious)
+
+Он нарушил свое обещание
+He broke his promise
+
+В итоге мы оказались здесь
+We ended up here
+
+Я приму участие в этом
+I will take part in this
+
+Это никак не связано с возрастом
+It has nothing to do with age
+
+Я говорил тебе что ничего не выйдет
+I told you it won't work
+
+Судя по всему я ошибся
+Apparently I was wrong
+
+Буду стараться изо всех сил
+I will strive to do my best
+I will do everything possible
+I will try my best
+
+Мне удалось достать дорогую машину
+I managed to get an expensive car
+
+Я согласился, а потом отказался
+I agreed, then refused
+I accepted and then rejected it
+I accepted then turned it down
+
+Нет смысла это делать
+It makes no sense to do this
+there's no point in doing it
+
+Произошло это в походе в школьном возрасте. Я взобрался на скалу и не видел возможности спуститься. Какое-то время я зависал на каменной стене. Я думал, что разобьюсь, но в конце концов нашел способ взобраться наверх.
+It happened on a hike at school age. I climbed a rock and saw no way to get down. For a while I hung on a stone wall. I thought I was going to crash, but in the end I found a way to climb up.
+`
